3501ec56ba1296d50d8da49a94038ec95aaa1364
pcox22 <10jonathancox@gmail.com>
2025-11-16 20:15:01 -0500

    New Navigation Integrated for all pages (except whatever I missed)

--- Ledgr/LedgrLogic/Navigation.cs
+++ Ledgr/LedgrLogic/Navigation.cs
@@ -20,9 +20,9 @@ public class Navigation
         _nav.NavigateTo($"/CoAList/{Username}");
     }
     
-    public void LedgerCall(string Username)
+    public void LedgerCall(string Username, string AccountNumber)
     {
-        _nav.NavigateTo($"/ledger/{Username}");
+        _nav.NavigateTo($"/ledger/{Username}/{AccountNumber}");
     }
     
     public void CoAForm(string Username)
@@ -46,6 +46,49 @@ public class Navigation
     public void JournalCall(string Username)
     {
         _nav.NavigateTo($"/journal/{Username}");
+    }
+
+    public void JournalEntryAdjustManager(string Username)
+    {
+        _nav.NavigateTo($"/adjustJournalEntryManager/{Username}");
+    }
+
+    public void Reports(string Username)
+    {
+        _nav.NavigateTo($"/managerReports/{Username}");
+    }
+
+    public void TrialBalanceCall(string Username)
+    {
+        _nav.NavigateTo($"/trialBalance/{Username}");
+    }
+
+    public void IncomeStatementCall(string Username)
+    {
+        _nav.NavigateTo($"/incomeStatement/{Username}");
+    }
+
+    public void BalanceSheetCall(string Username)
+    {
+        _nav.NavigateTo($"/balanceSheet/{Username}");
+    }
+
+    public void RetainedEarningsCall(string Username)
+    {
+        _nav.NavigateTo($"/retainedEarnings/{Username}");
+    }
+
+    public void AdjustJournal(string Username)
+    {
+        _nav.NavigateTo($"/JournalAdjustments/{Username}");
+    }
+    public void AdjustJournalEntry(string Username)
+    {
+        _nav.NavigateTo($"/adjustJournalEntry/{Username}");
+    }
+    public void JournalEntry(string Username)
+    {
+        _nav.NavigateTo($"/journalEntry/{Username}");
     }
     public void LogOut(string Username)
     {
